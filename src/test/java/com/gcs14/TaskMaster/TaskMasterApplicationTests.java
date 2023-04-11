e1197595c7ef052a574a3eb8d5d0ae58c616d676
Garrett Smith <lamelyte@gmail.com>
2023-04-10 20:19:43 -0500

    Initial commit

---
+++ src/test/java/com/gcs14/TaskMaster/TaskMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcs14.TaskMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
